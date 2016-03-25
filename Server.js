e0f9e30ea4849edbf41a6e2009bfc0fd8c2e074a
elribonazo <elribonazo@gmail.com>
2016-03-25 22:47:06 +0100

    REFACTOR

--- Server.js
+++ Server.js
@@ -1,25 +1,44 @@
+var SnakeClassServer = require('./lib/SnakeServer.js');
+var server = new SnakeClassServer({
+	'path':__dirname,
+	'domain':'localhost',
+	'port':8889,
+	'mapWidth':1000,
+	'mapHeight':1000
+});
 
-var fs = require('fs');
-var colors = require('colors');
-var events = require('events');
-var gameMapEngine = require('./GameMapEngine.js');
-var httpServer = require('./HTTPServer.js');
-var webSocketEngine = require('./WebSocketEngine.js');
-var gameEngine = require('./GameEngine.js');
-var MAP_HEIGHT = 50;					//Map height size in px 
-var MAP_WIDTH = 50;					//Map width size in px
-var MAP_FOOD = 10;						//0-100% value of food in map
-var httpPort = 80;
-var PLAYER_INITIAL_LENGHT = 4;
+server.start(function(self){
+	var clients = [];
+	/*
+	Se puede habilitar el evento
+	self.on("request", function(data){
+		console.log("REQUEST", data);
+	});
+	*/
+	self.io.on("connection",function(socket){
+
+		clients[socket.id] = socket;
+
+		socket.on('playerUpdate', function(data){
+			data.userId = socket.id;
+			self.gameEngine.movePlayer(data);
+		});															
+
+		socket.on('join', function(data){
+			data.userId = socket.id;
+			self.gameEngine.addPlayer(data);
+			socket.emit('mapUpdate',{'map':self.gameMapEngine.map});
+		});
+
+		socket.on('disconnect',function(){
+			if(clients[socket.id] !== undefined){
+				delete clients[socket.id];
+			}
+			self.gameEngine.removePlayer({'userId':socket.id});
+		});
+
+	});
 
 
-var httpServerInstance = new httpServer(httpPort);
-var webSocketEngineInstance = new webSocketEngine(httpServerInstance.getServer());
-var gameMapEngineInstance = new gameMapEngine(MAP_WIDTH,MAP_HEIGHT,MAP_FOOD);
-var gameEngineInstance = new gameEngine(gameMapEngineInstance,webSocketEngineInstance, PLAYER_INITIAL_LENGHT);
 
-
-//console.log(JSON.stringify(gameEngineInstance.mapGrid));
-
-
-
+});
