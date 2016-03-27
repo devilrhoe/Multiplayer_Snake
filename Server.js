97236b30e222e4f3f732c6141c9369b62ac4c88c
elribonazo <elribonazo@gmail.com>
2016-03-27 15:34:55 +0200

    Solucionado un problema con el merge

--- Server.js
+++ Server.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
 
-var fs = require('fs');
-var colors = require('colors');
-var events = require('events');
-var gameMapEngine = require('./GameMapEngine.js');
-var httpServer = require('./HTTPServer.js');
-var webSocketEngine = require('./WebSocketEngine.js');
-var gameEngine = require('./GameEngine.js');
-var MAP_HEIGHT = 2000;					//Map height size in px 
-var MAP_WIDTH = 2000;					//Map width size in px
-var MAP_FOOD = 20;						//0-100% value of food in map
-var httpPort = 8889;
-var PLAYER_INITIAL_LENGHT = 4;
-
-
-var httpServerInstance = new httpServer(httpPort);
-var webSocketEngineInstance = new webSocketEngine(httpServerInstance.getServer());
-var gameMapEngineInstance = new gameMapEngine(MAP_WIDTH,MAP_HEIGHT,MAP_FOOD);
-var gameEngineInstance = new gameEngine(gameMapEngineInstance,webSocketEngineInstance, PLAYER_INITIAL_LENGHT);
-
-
-//console.log(JSON.stringify(gameEngineInstance.mapGrid));
-
-
-
-=======
 var SnakeClassServer = require('./lib/SnakeServer.js');
 var server = new SnakeClassServer({
 	'path':__dirname,
@@ -69,4 +43,3 @@ server.start(function(self){
 
 
 });
->>>>>>> Canvas-testing
